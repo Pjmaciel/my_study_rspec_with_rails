a10d5185597cb173bd0c7bddf98237b517f50b56
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-05 06:20:34 -0300

    build: preparing the project for testing with controllers with views

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/users/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /users" do
+    it "works! (now write some real specs)" do
+      get users_path
+      expect(response).to have_http_status(200)
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/users/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
