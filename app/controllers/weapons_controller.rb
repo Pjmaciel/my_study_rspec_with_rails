8092b1fb4ca5522955f775c096b1fb6c2035a660
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-05 15:51:41 -0300

    build: structure for create,index,show and destroy

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,4 +1,26 @@
 class WeaponsController < ApplicationController
   def index
+    @weapons = Weapon.all
   end
+
+  def create
+    @Weapon = Weapon.create(Weapon_params)
+    redirect_to weapons_path, notice: 'Weapon was created successfully'
+  end
+
+  def destory
+    @weapon = Weapon.find(params[:id])
+    @weapon.destory
+    redirect_to weapons_path, notice: 'Weapon was successfully destroyed.'
+  end
+
+  def show
+    @weapon = Weapon.find(params[:id])
+  end
+
+  private
+    def Weapon_params
+      param.require(:Weapon).permit(:name, :description, :power_base, :power_step, :level)
+    end
+
 end
