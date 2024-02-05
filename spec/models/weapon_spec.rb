654b5b98a3a2ccac505e7fec9975da86745c3fdf
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-04 23:25:32 -0300

    test: improve tests for model weapon

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 RSpec.describe Weapon, type: :model do
   it "returns the weapon current power" do
     weapon = build(:weapon)
-    expected_weapon = power_base + ((level - 1) * power_step)
-    expect(weapon.current_power).to eq(expected_weapon)
+    expected_power = weapon.power_base + ((weapon.level - 1) * weapon.power_step)
+    expect(weapon.current_power).to eq(expected_power)
   end
 
   it "returns the correct hero title" do
+    name = FFaker::Game
+    level = FFaker::Random.rand(1..99)
+    
+    weapon = create(:weapon, name: name, level: level)
 
+    expect(weapon.title).to eq("#{name} ##{level}")
   end
 end
