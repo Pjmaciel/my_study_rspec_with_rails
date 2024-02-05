66aacd8ee4f1dc74ddbb67a3247fa6e8c1c8aa41
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-04 22:49:16 -0300

    chore: adding methods to the user and weapon models

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,2 +1,12 @@
 class Weapon < ApplicationRecord
+
+     #this level show the power now of weapon
+    def current_power
+        (self.power_base + ((level - 1) * self.power_step))
+    end
+
+    def title
+        "#{self.name} ##{self.level}"
+    end
+
 end
