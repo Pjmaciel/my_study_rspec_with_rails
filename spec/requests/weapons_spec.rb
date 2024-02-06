cd3065efbf6e4762af1a5e1a10b0e6ed9edd6bf0
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-06 05:51:49 -0300

    build: structure for request for weapons

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -1,11 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "Weapons", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/weapons/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /weapons" do
+    it "returns http success status" do
+      get weapons_path
+      expect(response).to have_http_status(200)  
     end
+  end
+    
+    it "the wapons`s information presents" do
+      weapons = create_list(:weapon,3)
+
+      get weapons_path
+
+      weapons.each do |weapon|
+        expect(response.body).to include(weapon.name, weapon.current_power, weapon.power_base)
+      end
+    end
+
+
+  describe "POST /weapons" do
+    context "when it has valid parameters" do
+      it "creates the weapon with correct attributes"
+    end
+
+    context "when it has no valid parameters" do
+      it "does not create user"
+    end
+  end
+
+  describe "SHOW /weapon/:id" do
+    it "Show details of weapon by Id"
+  end
+
+  describe "DESTROY /weapon/:id" do
+    it "Delete the weapon by id"      
   end
 
 end
