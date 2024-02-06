d0e56d2e90e54c64158b1c2f7f8aa1774d7c22c2
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-06 08:57:00 -0300

    Test: Create weapons with and without params

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -22,11 +22,22 @@ RSpec.describe "Weapons", type: :request do
 
   describe "POST /weapons" do
     context "when it has valid parameters" do
-      it "creates the weapon with correct attributes"
+      it "creates the weapon with correct attributes" do
+        # Generate a hash of attributes for a new user
+        weapon_attributes = FactoryBot.attributes_for(:weapon)
+        # Perform a POST request to the route with the user attributes
+        post weapons_path, params:{ weapon: weapon_attributes}
+        # Verify if the user has the correct attributes
+        expect(Weapon.last).to have_attributes(weapon_attributes)  
+      end
     end
 
     context "when it has no valid parameters" do
-      it "does not create user"
+      it "does not create user" do
+         expect{
+          post weapons_path, params: { weapon: {name: '', description: '', power_base: '', power_step: '', level: ''}}
+         }.to_not change(Weapon, :count)
+      end
     end
   end
 
