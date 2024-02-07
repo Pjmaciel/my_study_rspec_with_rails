d7fd8262c5d77704f3aa2c4ffbd591a59b0b1905
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-07 10:32:59 -0300

    test: when the enemy exists it returns status 200

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :create]
   resources :weapons, only: [:index, :create, :show, :destroy]
-  resources :enmies, only: [:update, :destory]
+  resources :enemies, only: [:update, :destroy]
 end
