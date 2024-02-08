d723029fa6bdd658d5f4ee993c39bb7ac84f7795
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-08 20:33:35 -0300

    test: Show deatails of enemy by Id

--- spec/requests/enemies_spec.rb
+++ spec/requests/enemies_spec.rb
@@ -10,6 +10,17 @@ RSpec.describe "Enemies", type: :request do
   end
 
   #create
+
+  #show
+  describe "SHOW /enemy/:id" do
+    it "Show details of enemy by Id" do
+        enemy = create(:enemy)
+        get enemy_path(enemy) 
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include(enemy.name, enemy.power_base.to_s, enemy.power_step.to_s, enemy.level.to_s)
+      end
+  end 
+
   
 
   
