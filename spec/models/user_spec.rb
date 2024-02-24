8d3cc79108587ecc2425a25040c50deb9159df26
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-24 16:54:34 -0300

    test: new name test 2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
    end
 
   it "returns the correct hero title" do
-    nickname = "pjma"
+    nickname = "leandro"
     kind = %w[knight wizard].sample
     level = FFaker::Random.rand(1..99)
 
